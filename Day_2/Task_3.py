69ec80cca52afe1ea0c8870a0dd735bdab489d88
shiamsharif <shiam.sharif.07@gmail.com>
2023-07-28 19:15:53 +0600

    task3 done

---
+++ Day_2/Task_3.py
@@ -0,0 +1,11 @@
+# 🚨 Don't change the code below 👇
+age = input("What is your current age? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+
+Left_age = 90 - int(age)
+
+print("You have " + str(Left_age * 365) + " days," +  str(Left_age * 52) +" weeks, and " + str(Left_age * 12) + " months left.")
+
+#You have 12410 days, 1768 weeks, and 408 months left.
---
+++ Day_2/mathmatics.py
@@ -0,0 +1,14 @@
+print(type(3+5))
+
+print(type(3-5))
+
+print(type(3*5))
+
+print(type(10/5))
+
+print(type(2 ** 2))
+
+# print(type(3+5))
+
+
+
