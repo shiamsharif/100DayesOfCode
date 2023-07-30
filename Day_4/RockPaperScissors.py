b0346596452faeea330ef8d085958ecd01e95c9a
shiamsharif <shiam.sharif.07@gmail.com>
2023-07-31 03:54:43 +0600

    bug solved.

--- Day_4/RockPaperScissors.py
+++ Day_4/RockPaperScissors.py
@@ -33,32 +33,36 @@ game_img = [rock, paper, scissors]
 
 #Write your code below this line 👇
 user_choice = int(input("What do you choose? Type 0 for Rock, 1 for Paper or @ for Scissors.\n"))
-print(game_img[user_choice])
 
 
-computer_choice = random.randint(0,2)
-print("Computer chose :")
-print(game_img[computer_choice])
-
-
-#condition:
-    # Rock wins against scissors.
-    # Scissors win against paper.
-    # Paper wins against rock.
-
-
-#condition
 if  user_choice >= 3 or user_choice < 0:
     print("You chose an invalid number. You lose")
-elif user_choice == 0 and computer_choice == 2:
-    print("You Win!")
-elif computer_choice == 0 and user_choice == 2:
-    print("You Lose") 
-elif computer_choice > user_choice:
-    print("You Lose")
-elif user_choice > computer_choice:
-    print("You Win!")
-elif computer_choice == user_choice:
-    print("It's a draw")
-elif  user_choice >= 3 or user_choice <= 0:
-    print("You chose an invalid number. You lose")
+else:
+    print(game_img[user_choice])
+
+
+    computer_choice = random.randint(0,2)
+    print("Computer chose :")
+    print(game_img[computer_choice])
+
+
+    #condition:
+        # Rock wins against scissors.
+        # Scissors win against paper.
+        # Paper wins against rock.
+
+
+    #condition
+
+    if user_choice == 0 and computer_choice == 2:
+        print("You Win!")
+    elif computer_choice == 0 and user_choice == 2:
+        print("You Lose") 
+    elif computer_choice > user_choice:
+        print("You Lose")
+    elif user_choice > computer_choice:
+        print("You Win!")
+    elif computer_choice == user_choice:
+        print("It's a draw")
+    elif  user_choice >= 3 or user_choice <= 0:
+        print("You chose an invalid number. You lose")
