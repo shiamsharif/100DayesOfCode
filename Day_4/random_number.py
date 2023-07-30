d2bfe9701e8b10f0f42b10e497f5a6fdf8c55a4d
shiamsharif <shiam.sharif.07@gmail.com>
2023-07-31 02:15:11 +0600

    Day_4 50% done

---
+++ Day_4/random_number.py
@@ -0,0 +1,11 @@
+import random
+
+
+random_integer = random.randint(1, 6)
+print(random_integer)
+
+random_float = random.random() * 5
+print(random_float)
+
+love_score = random.randint(1 , 100)
+print(f"Your love number is {love_score}")
