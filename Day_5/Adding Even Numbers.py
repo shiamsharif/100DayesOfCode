0680d4c66d8982b0a4ed6c57ca873b9e05caa90d
shiamsharif <shiam.sharif.07@gmail.com>
2023-08-01 06:19:57 +0600

    Day 5 done

---
+++ Day_5/Adding Even Numbers.py
@@ -0,0 +1,13 @@
+# #Write your code below this row 👇
+total = 0
+for number in range(2 , 101, 2):
+    total = total + number
+print(total)
+
+
+#For loop and If
+new_total = 0
+for num in range(1,101,):
+    if num % 2 == 0:
+        new_total += num
+print(new_total)
---
+++ Day_5/for loop.py
@@ -0,0 +1,10 @@
+# numbers = ["10", "7", "20", "16"]
+# for number in numbers:
+#     print(number)
+
+total  = 0
+for number in range(1 , 101):
+    total = total + number
+    print(f" {number} +")
+print("------------------")
+print(f"Total: {total}")
