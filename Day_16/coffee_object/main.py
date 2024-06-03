a1cb513c70a14c2930a2cd4ca49e4932f4c4f561
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-03 23:41:18 +0600

    Day: 16

--- Day_16/coffee_object/main.py
+++ Day_16/coffee_object/main.py
@@ -10,7 +10,7 @@ is_on = True
 
 while is_on:
     options = menu.get_items()
-    choice = input(f"What would you like? ({options}): ")
+    choice = input(f"what would you like? ({options}): ")
     print(choice)
     if choice == "off":
         is_on = False
@@ -20,4 +20,26 @@ while is_on:
     else:
         drink = menu.find_drink(choice)
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
-                coffee_maker.make_coffee(drink)
+            coffee_maker.make_coffee(drink)
+
+
+
+# money_machine = MoneyMachine()
+# coffee_maker = CoffeeMaker()
+# menu = Menu()
+
+# is_on = True
+
+# while is_on:
+#     options = menu.get_items()
+#     choice = input(f"What would you like? ({options}): ")
+#     print(choice)
+#     if choice == "off":
+#         is_on = False
+#     elif choice == "report":
+#         coffee_maker.report()
+#         money_machine.report()
+#     else:
+#         drink = menu.find_drink(choice)
+#         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+#                 coffee_maker.make_coffee(drink)
