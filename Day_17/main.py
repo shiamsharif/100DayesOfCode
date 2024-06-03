f9406fc92cb96b1d9cc55d4c53493708c34929c1
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-04 00:17:52 +0600

    Day: 17.3

---
+++ Day_17/main.py
@@ -0,0 +1,22 @@
+class User:
+    def __init__(self,id,name):
+        self.id = id
+        self.name = name
+
+    def Print(self):
+        print(f"My name is {self.name} and id {self.id}")
+
+
+user_1 = User("41220100032", "shiam")
+user_1.Print()
+
+
+# print(user_1.username)
+
+
+
+# user_2 = User()
+# user_2.id = "41220100032"
+# user_2.username = "Shiam"
+
+# print(user_2.username)
