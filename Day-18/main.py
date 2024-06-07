03de60d55d49451a52a2f7778a7dc6c6a458603b
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-07 20:58:15 +0600

    Day-18 vdo-5

--- Day-18/main.py
+++ Day-18/main.py
@@ -1,13 +1,44 @@
-######## Challenge 1 - Draw a Square ############
+"""######## Challenge 1 - Draw a Square ############"""
 
 from turtle import Screen, Turtle
 
 the_turtle = Turtle()
 
 
-for _ in range(4):
-    the_turtle.forward(100)
-    the_turtle.right(90)
+# for _ in range(4):
+#     the_turtle.forward(100)
+#     the_turtle.right(90)
+
+
+
+
+"""#######Challenge 2 - Draw a Dashed Line ########"""
+
+## From stackOverFlow
+# for _ in range(15):
+#     the_turtle.forward(10)
+#     the_turtle.color("white")
+#     the_turtle.forward(10)
+#     the_turtle.color("black")
+
+
+# # From Angela Yo
+# for _ in range(15):
+#     the_turtle.forward(10)
+#     the_turtle.penup()
+#     the_turtle.forward(10)
+#     the_turtle.pendown()
+
+
+
+
+
+
+
+
+
+
+
 
 
 screen = Screen()
