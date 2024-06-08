c021341089bbced25068135834d301ebea6f4061
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-08 12:03:00 +0600

    Day-18 vdo-8

--- Day-18/main.py
+++ Day-18/main.py
@@ -1,9 +1,11 @@
-"""######## Challenge 1 - Draw a Square ############"""
 
 from turtle import Screen, Turtle
+import random
 
 the_turtle = Turtle()
 
+
+"""######## Challenge 1 - Draw a Square ############"""
 
 # for _ in range(4):
 #     the_turtle.forward(100)
@@ -85,19 +87,79 @@ the_turtle = Turtle()
 #     the_turtle.right(36)
 
 
-def draw_shape(num_sides):
-    angle = 360 / num_sides
-    for _ in range(num_sides):
-        the_turtle.forward(100)
-        the_turtle.right(angle)
+"""Form Angela Yu"""
+# colors = ["Red", "Blue", "Green", "Yellow", "Purple", "Orange", "Pink", "Brown", "Black","Cyan", "Magenta", "Lime", "Teal", "Navy", "Maroon", "Olive", "Silver", "Gold", "Coral"]
 
 
-for shape_side_n in range(3,11):
-    draw_shape(shape_side_n)
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         the_turtle.forward(100)
+#         the_turtle.right(angle)
+
+
+# for shape_side_n in range(3,11):
+#     the_turtle.color(random.choice(colors))
+#     draw_shape(shape_side_n)
 
 
 
- 
+"""########### Challenge 4 - Draw a Random Walk ########"""
+
+# angle = [0, 90, 180,270]
+
+# colors = ["Red", "Blue", "Green", "Yellow", "Purple", "Orange", "Pink", "Brown", "Black","Cyan", "Magenta", "Lime", "Teal", "Navy", "Maroon", "Olive", "Silver", "Gold", "Coral"]
+
+# the_turtle.pensize(15)
+# for shape_side_n in range (200):
+#     the_turtle.color(random.choice(colors))
+#     the_turtle.forward(30)
+#     the_turtle.right(random.choice(angle))
+
+
+"""########### Challenge 5 - Draw a Random Walk and Color ########"""
+
+"""My Solution"""
+# import turtle 
+# turtle.colormode(255)
+
+# def random_color():
+#     r = random.randint(0,255)
+#     g = random.randint(0,255)
+#     b = random.randint(0,255)
+#     return r,g,b
+
+
+# direction = [0, 90, 180, 270]
+# the_turtle.pensize(15)
+# the_turtle.speed("fastest")
+
+# for _ in range(200):
+#     r, g, b = random_color()
+#     the_turtle.pencolor((r,g,b))  
+#     the_turtle.forward(30)
+#     the_turtle.setheading(random.choice(direction))
+
+"Angela Yu Solution"
+import turtle 
+turtle.colormode(255)
+
+def random_color():
+    r = random.randint(0,255)
+    g = random.randint(0,255)
+    b = random.randint(0,255)
+    random_color = (r,g,b)
+    return random_color
+
+
+direction = [0, 90, 180, 270]
+the_turtle.pensize(15)
+the_turtle.speed("fastest")
+
+for _ in range(200):
+    the_turtle.pencolor(random_color())  
+    the_turtle.forward(30)
+    the_turtle.setheading(random.choice(direction))
 
 
 
