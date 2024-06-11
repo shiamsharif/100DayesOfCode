8c7512a1a126c461476e5f74b430312a9e6808cb
smabid <hasan.abid56@yahoo.com>
2024-06-11 16:37:49 +0600

    Day-20 vdo-2

--- Day-20/Snake_game/main.py
+++ Day-20/Snake_game/main.py
@@ -5,16 +5,21 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
-segment_1 = Turtle("square")
-segment_1.color("red")
+starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
-segment_2 = Turtle("square")
-segment_2.color("white")
-segment_2.goto(-20, 0)
+for position in starting_positions:
+    new_segment = Turtle("square")
+    # new_segment.color("white")
+    if position[0] == 0 and position[1] == 0:
+        new_segment.color("red")
+    else:
+        new_segment.color("white")
+    new_segment.goto(position)
+    
 
-segment_3 = Turtle("square")
-segment_3.color("white")
-segment_3.goto(-40, 0)
+    
+
+
 
 
 
