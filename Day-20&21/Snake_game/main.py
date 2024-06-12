fc6556403a95a4dcea168902200baf5c6bec2d06
smabid <hasan.abid56@yahoo.com>
2024-06-12 13:11:54 +0600

    Day-20 vdo-3

--- Day-20&21/Snake_game/main.py
+++ Day-20&21/Snake_game/main.py
@@ -1,5 +1,6 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from snake import Snake
+from food import Food
 import time
 
 """ Create Game Display OR Screen """
@@ -10,6 +11,7 @@ screen.title("My Snake Game")
 screen.tracer(0)
  
 snake = Snake()
+food =  Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -22,8 +24,10 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    # Detect collision with food.
+    if snake.head.distance(food) < 15:
+        food.refresh()
  
 screen.exitonclick()
