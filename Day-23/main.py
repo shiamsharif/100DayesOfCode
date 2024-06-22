dc26cc0be8815ef8efefd8e0b851e35453cbb625
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-22 18:43:49 +0600

    Day-23 vdo-9

--- Day-23/main.py
+++ Day-23/main.py
@@ -9,6 +9,7 @@ screen.setup(width=600, height=600)
 screen.tracer(0)
 
 player = Player()
+car_manager = CarManager()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -17,5 +18,14 @@ game_is_on = True
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    
+    car_manager.create_car()
+    car_manager.move_car()
+
+    """ Detect collision with car """
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
 
 
+screen.exitonclick()
