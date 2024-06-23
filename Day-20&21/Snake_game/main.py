ba5af040b7c0bd3af0f60a2b83fe045072e81d6c
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-23 11:57:14 +0600

    Day-24 vdo-2

--- Day-20&21/Snake_game/main.py
+++ Day-20&21/Snake_game/main.py
@@ -36,13 +36,15 @@ while game_is_on:
 
     # Detect collision with wall.
     if snake.head.xcor() > 280 or snake.head.xcor() < -300 or snake.head.ycor() > 290 or snake.head.ycor() < -290:
-         game_is_on = False
-         scoreboard.game_over()
+         scoreboard.reset()
+         snake.reset( )
 
-    # Detect collision with wall.
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+    # Detect collision with tail.
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+           scoreboard.reset() 
+           snake.reset()
  
 screen.exitonclick()
