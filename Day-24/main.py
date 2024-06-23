64e5118fe6746bd18a81715481a350909026ed40
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-23 16:09:12 +0600

    Day-24 vdo-5

--- Day-24/main.py
+++ Day-24/main.py
@@ -1,7 +1,4 @@
-# with open("file.txt") as file:
-#     contents = file.read()
-#     print(contents)
+with open("../Day-20&21/Snake_game/data.txt") as file:
+    contents = file.read()
+    print(contents)
 
-
-with open("file2.txt", mode="a") as file:
-    file.write("shiam sharif.\nshaman sharif")
