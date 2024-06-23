5649ec1fc9fbee6a15d658b1e0aec34c5163a30e
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-23 18:06:11 +0600

    day-24

--- Day-24/Mail_merge/main.py
+++ Day-24/Mail_merge/main.py
@@ -9,18 +9,19 @@
     
 #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
 
-#  firstTry
-"""
-with open("../Mail_merge/Input/Names/invited_names.txt") as name:
-    each_name = name.readline()
-    # print(each_name)
+"""  firstTry """
 
-with open("../Mail_merge/Input/Letters/starting_letter.txt") as letter:
-    letter1 = letter.read()
-    x = letter1.replace("[name]", f"{each_name}")
-    print(x)
-"""
+# with open("../Mail_merge/Input/Names/invited_names.txt") as name:
+#     each_name = name.readline()
+#     # print(each_name)
 
+# with open("../Mail_merge/Input/Letters/starting_letter.txt") as letter:
+#     letter1 = letter.read()
+#     x = letter1.replace("[name]", f"{each_name}")
+#     print(x)
+
+
+"""  second try and won """
 with open("../Mail_merge/Input/Names/invited_names.txt") as invited_name:
     name_list = invited_name.readlines()
     names = [item.replace("\n", "") for item in name_list]
@@ -35,5 +36,17 @@ for name in names:
             f.write(x)
         print(f"Created {filename}")
 
-  
-    
+
+""" Angela's Solution with bug"""
+# PLACEHOLDER = "[name]"
+
+# with open("./Input/Names/invited_names.txt") as names_file:
+#     names = names_file.readlines()
+
+# with open("./Input/Letters/starting_letter.txt") as letter_file:
+#     letter_contents = letter_file.read()
+#     for name in names:
+#         stripped_name = name.strip()
+#         new_latter = letter_contents.replace(PLACEHOLDER, stripped_name)
+#         with open(f"./Output/Angela/letterFor{name}.txt", mode="w") as completed_letter:
+#             completed_letter.write(new_latter)
