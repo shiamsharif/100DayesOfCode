f11ba8f32f26a367c17cb2d04cd5b78a343659e4
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-24 16:34:26 +0600

    Day-25 CSV

---
+++ Day-25/main.py
@@ -0,0 +1,19 @@
+""" 1 """
+# with open("./002 weather-data.csv") as weather_data:
+#     data = weather_data.readlines()
+
+
+""" 2 """
+# import csv
+
+# with open("./002 weather-data.csv") as data_File:
+#     data = csv.reader(data_File)
+#     temperatures = []
+#     for row in data:
+#         try:
+#             temperatures.append(int(row[1]))
+#         except ValueError:
+#             pass
+ 
+# print(temperatures)
+
