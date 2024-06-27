374192423625aa2559cb7d1a459d98076089a997
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-27 22:51:07 +0600

    Day-26 vdo-5

--- Day-26/List_Comprehension/main.py
+++ Day-26/List_Comprehension/main.py
@@ -25,6 +25,34 @@
 
 
 """ Exercise-2 """
-numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-result = [number for number in numbers if number % 2 == 0]
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# result = [number for number in numbers if number % 2 == 0]
+# print(result)
+
+
+""" Exercise-3 """
+""" Solution: 1 """
+# import pandas as pd
+
+# # Read the text files into DataFrames
+# datafile1 = pd.read_csv("file1.txt", header=None, names=['value'])
+# datafile2 = pd.read_csv("file2.txt", header=None, names=['value'])
+
+# # Convert the 'value' column to a set of integers
+# set1 = set(datafile1['value'])
+# set2 = set(datafile2['value'])
+
+# # Find the intersection of both sets
+# common_values = set1.intersection(set2)
+
+# print("Common integers in both files:", common_values)
+
+
+""" Solution: 2 """
+with open("./file1.txt") as file1:
+    file_1_Data = file1.readlines()
+with open("./file2.txt") as file2:
+    file_2_Data = file2.readlines()
+    
+result = [int(num) for num in file_1_Data if num in file_2_Data]
 print(result)
