89e76cf2ce617ed0468d2e4e5b48ae64d46cdcb6
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-27 20:46:48 +0600

    Day-26  Exercise-2 done

--- Day-26/List_Comprehension/main.py
+++ Day-26/List_Comprehension/main.py
@@ -18,6 +18,13 @@
 # print(short_name) 
 
 
+""" Exercise-1 """
+# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# squared_numbers = [number**2 for number in numbers]
+# print(squared_numbers)
+
+
+""" Exercise-2 """
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-squared_numbers = [number**2 for number in numbers]
-print(squared_numbers)
+result = [number for number in numbers if number % 2 == 0]
+print(result)
