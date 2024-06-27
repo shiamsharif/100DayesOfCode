347373f19ba4384806622a9815685c696a5c83a8
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-28 00:51:21 +0600

    Day-26 vdo-10

--- Day-26/Dictionary_Comprehension/main.py
+++ Day-26/Dictionary_Comprehension/main.py
@@ -8,9 +8,31 @@
 # print(passed_Students)
 
 """ Exercise-1 """
-sentence = "What is the airspeed velocity of an unladen swallow?"
-result = {sentence:len(sentence) for sentence in sentence.split()}
-print(result) 
+# sentence = "What is the airspeed velocity of an unladen swallow?"
+# result = {sentence:len(sentence) for sentence in sentence.split()}
+# print(result) 
 
-x = {name:((value * 9 / 5) + 32) for (name, value) in result.items()}
-print(x)
+# x = {name:((value * 9 / 5) + 32) for (name, value) in result.items()}
+# print(x)
+
+
+""" How to iterate over a pandas dateframe """
+Students = {
+    "student" : ["shiam", "CR7", "Messi"],
+    "score" : [84,3,2]
+}
+
+for (key, value) in Students.items():
+    print(value)
+
+
+import pandas
+
+students_data_frame = pandas.DataFrame(Students)
+""" Create a file and append data in this file """
+# stringdata = students_data_frame.to_string()
+# with open("../Dictionary_Comprehension/file.txt", mode='w') as file1:
+#     file1.write(stringdata) 
+for (index, row) in students_data_frame.iterrows():
+    if row.student == "shiam":
+        print(row.score)
