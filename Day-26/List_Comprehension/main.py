dc422f18d3d08f308eabe8f1ec28ebbbcf7f85b0
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-27 20:36:47 +0600

    Day-26 vdo-2

---
+++ Day-26/List_Comprehension/main.py
@@ -0,0 +1,18 @@
+# num = [1,2,3]
+# new_list = [n+1 for n in num]
+# print(new_list)
+
+
+# name = "shiam"
+# new_list = [n for n in name]
+# print(new_list)
+
+
+# list = [n*2 for n in range(1,5)]
+# print(list)
+
+
+""" Conditional List Comprehension """
+names = ["Sam", "Alex", "Jo", "shiam", "shaman", "Savrin","Fa", "Zarifah"]
+short_name = [name.upper() for name in names if not len(name) < 5]
+print(short_name)
