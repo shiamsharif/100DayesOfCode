e9cdf5c58f469a1e602d3206c5a29ee5c2043cab
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-28 20:50:54 +0600

    Day-27 vdo-4

--- Day-27/main.py
+++ Day-27/main.py
@@ -7,10 +7,13 @@ window.minsize(width=500, height=300)
 
 """ Label """
 my_label = tkinter.Label(text="ShiamSharif", font=("Comic Sans MS", 24, "bold"))
-my_label.pack()
+my_label.pack(side="left")
 
 
+import turtle
 
+tim = turtle.Turtle()
+tim.write("some text", font=("Comic Sans MS", 24, "bold"))
 
 
 
