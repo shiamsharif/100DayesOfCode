41f8e32abb2f04c420b3b323e51a4e3f6db48e95
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-29 19:38:05 +0600

    Day-27 vdo-8

--- Day-27/main.py
+++ Day-27/main.py
@@ -26,10 +26,19 @@ button.pack()
 input = Entry()
 input.pack()
 
+""" Text """
+text = Text(height=5, width=30)
+# put cursor in testbox.
+text.focus()
+# add some text to begin with.
+text.insert(END, "Example of multi-line text entry.")
+# gets the current value in textbox at line 1, character 0
+print(text.get("1.0", END))
+text.pack()
 
-
-
-
+""" Spinbox """
+def spinbox_used():
+    
  
 
 window.mainloop()
