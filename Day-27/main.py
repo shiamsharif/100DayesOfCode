06606b53c90a2a6467b81b154a45d6d7113fa2bd
shiamsharif <shiam.sharif.07@gmail.com>
2024-06-29 19:54:18 +0600

    Day-27 vdo-9 challenge Done

--- Day-27/main.py
+++ Day-27/main.py
@@ -1,5 +1,11 @@
 from tkinter import *
 
+def clicked():
+    new_text = input.get()
+    my_label.config(text=new_text)
+    print("I got Clicked.")
+
+
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
@@ -7,38 +13,37 @@ window.minsize(width=500, height=300)
 
 """ Label """
 my_label = Label(text="ShiamSharif", font=("Comic Sans MS", 24, "bold"))
-my_label.pack()
+my_label.grid(column=0, row=0)
 
 my_label["text"] = "New Text"
 
 
 """ Button """
-def clicked():
-    new_text = input.get()
-    my_label.config(text=new_text)
-    print("I got Clicked.")
-
 button = Button(text="Click me", command=clicked)
-button.pack()
+button.grid(column=1, row=1)
+
+""" New Button """
+New_button = Button(text="Press me", command=clicked)
+New_button.grid(column=2, row=0 )
 
 
 """ Entry """
 input = Entry()
-input.pack()
+input.grid(column=3, row=3)
 
 """ Text """
-text = Text(height=5, width=30)
-# put cursor in testbox.
-text.focus()
-# add some text to begin with.
-text.insert(END, "Example of multi-line text entry.")
-# gets the current value in textbox at line 1, character 0
-print(text.get("1.0", END))
-text.pack()
+# text = Text(height=5, width=30)
+# # put cursor in testbox.
+# text.focus()
+# # add some text to begin with.
+# text.insert(END, "Example of multi-line text entry.")
+# # gets the current value in textbox at line 1, character 0
+# print(text.get("1.0", END))
+# text.pack()
 
-""" Spinbox """
-def spinbox_used():
-    print(spinbox.get())
+# """ Spinbox """
+# def spinbox_used():
+#     print(spinbox.get())
 
  
 
