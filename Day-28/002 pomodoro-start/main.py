d17fcf69bad0ada805a6d0895ed8d2577e641a2f
shiamsharif <shiam.sharif.07@gmail.com>
2024-07-02 02:09:28 +0600

    Day-28 Done

--- Day-28/002 pomodoro-start/main.py
+++ Day-28/002 pomodoro-start/main.py
@@ -10,7 +10,18 @@ WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
+timer = None
+
+
 # ---------------------------- TIMER RESET ------------------------------- # 
+def reset_timer():
+    window.after_cancel(timer)
+    canvas.itemconfig(timer_text, text="00:00")
+    title_lable.config(text="Timer")
+    check_mark.config(text="")
+    global reps
+    reps = 0
+
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
@@ -33,7 +44,6 @@ def start_timer():
 
    
 
-
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 def count_down(conut):
 
@@ -45,18 +55,28 @@ def count_down(conut):
 
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if conut > 0:
-        window.after(1000, count_down, conut - 1)
+        global timer
+        timer = window.after(1000, count_down, conut - 1)
     else:
         start_timer()
+        marks = ""
+        work_sessions = math.floor(reps/2)
+        for _ in range(work_sessions):
+            marks += "✓"
+        check_mark.config(text=marks)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
+# Screen
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+# Main Title
 title_lable = Label(text="Timer", fg=GREEN, bg=YELLOW ,font=("FONT_NAME", 50))
 title_lable.grid(column=1, row=0)
 
+# Image 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
@@ -67,11 +87,12 @@ canvas.grid(column=1, row=1)
 start_button = Button(text="Start", highlightthickness=0, command=start_timer)
 start_button.grid(column=0, row=2)
 
-reset_button = Button(text="Reset", highlightthickness=0)
+reset_button = Button(text="Reset", highlightthickness=0, command=reset_timer)
 reset_button.grid(column=2, row=2)
 
-check_mark = Label(text="✓", fg=GREEN, bg=YELLOW, font=("FONT_NAME", 30))
+check_mark = Label(text="", fg=GREEN, bg=YELLOW, font=("FONT_NAME", 30))
 check_mark.grid(column=1, row=2)
+
 
 
 
