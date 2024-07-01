06bcbd4cc38e7922dca16994091b7e463de51dd9
shiamsharif <shiam.sharif.07@gmail.com>
2024-07-01 22:02:42 +0600

    Day-28 vdo-3

--- Day-28/002 pomodoro-start/main.py
+++ Day-28/002 pomodoro-start/main.py
@@ -20,16 +20,23 @@ window = Tk()
 window.title("Prmodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
 
+title_lable = Label(text="Timer", fg=GREEN, bg=YELLOW ,font=("FONT_NAME", 50))
+title_lable.grid(column=1, row=0)
 
 canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100,130, text="00:00", fill="white", font=("Font_Name", 35, "bold"))
-canvas.pack()
+canvas.create_text(100,130, text="00:00", fill="white", font=("FONT_NAME", 35, "bold"))
+canvas.grid(column=1, row=1)
 
+start_button = Button(text="Start", highlightthickness=0)
+start_button.grid(column=0, row=2)
 
+reset_button = Button(text="Reset", highlightthickness=0)
+reset_button.grid(column=2, row=2)
 
-
+check_mark = Label(text="✓", fg=GREEN, bg=YELLOW, font=("FONT_NAME", 30))
+check_mark.grid(column=1, row=2)
 
 
 
