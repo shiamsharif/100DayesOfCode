82cbaec641d742969aa3274c619e7560a24af8d7
shiamsharif <shiam.sharif.07@gmail.com>
2024-07-02 01:37:29 +0600

    Day-28 vdo-6

--- Day-28/002 pomodoro-start/main.py
+++ Day-28/002 pomodoro-start/main.py
@@ -6,15 +6,32 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 25
+WORK_MIN = 1
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
-
+reps = 0
 # ---------------------------- TIMER RESET ------------------------------- # 
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+
+    work_sec = WORK_MIN * 60
+    short_break_se = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_lable.config(text="Long Break", fg=RED, bg=YELLOW ,font=("FONT_NAME", 40))
+    elif reps % 2 == 0:
+        count_down(short_break_se)
+        title_lable.config(text="Break", fg=PINK, bg=YELLOW ,font=("FONT_NAME", 40))
+    else:
+        count_down(work_sec)
+        title_lable.config(text="Work", fg=GREEN, bg=YELLOW ,font=("FONT_NAME", 40))
+
+   
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
@@ -29,6 +46,8 @@ def count_down(conut):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if conut > 0:
         window.after(1000, count_down, conut - 1)
+    else:
+        start_timer()
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
